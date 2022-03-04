5879d3c877fa138fddd31023b1d77a3113937c55
tavarina <fabi.ana_deoliveira@outlook.com>
2022-03-04 17:33:14 -0300

    Projeto react

---
+++ exercicios/src/componentes/Primeiro.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+//Possibilidades de escrever funçoes
+
+
+export default () =>  <h1>Primeiro</h1>
+
+
+
+// export default function() {
+//     return <h1>Primeiro</h1> //assim é com o transparer
+// }
+
+
+
+
+// export default function() {
+//     return "Test" //assim é a forma básica
+// }
--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-const elemento = <h1>React2 </h1>
-ReactDom.render(elemento, document.getElementById('root'))
+import Primeiro from './componentes/Primeiro'
+
+// const elemento = <h1>React2 </h1>
+
+ReactDom.render(<Primeiro/>, document.getElementById('root'))
+
+
+// ReactDom.render(elemento, document.getElementById('root'))
 // ReactDom.render(<h1>React</h1>, document.getElementById('root'))
