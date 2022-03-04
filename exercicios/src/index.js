92513206e65cf0a87d807da65e80023ee234a76d
tavarina <fabi.ana_deoliveira@outlook.com>
2022-03-04 14:09:37 -0300

    Projeto react

---
+++ exercicios/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const elemento = <h1>React2 </h1>
+ReactDom.render(elemento, document.getElementById('root'))
+// ReactDom.render(<h1>React</h1>, document.getElementById('root'))
